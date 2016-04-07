1b70189f4210bf59956c38347ec14d523355b3e1
Will Ballard <wballard@mailframe.net>
2016-04-07 17:01:33 -0500

    it can reply at all

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ Errors fire an `error` event, or if there is no handler, stream to `console.erro
 */
 module.exports =
   class HipchatBot extends botframework.DialogCollection {
-    constructor(options) {
+    constructor (options) {
       super(options)
       this.options = options
+      this.options.defaultDialogId = '/'
+      this.options.sessionStore = new botframework.MemoryStorage()
     }
 
-    connect() {
+    connect () {
       return new XmppClient({
         jid: this.options.uid,
         password: this.options.pwd,
@@ -29,17 +31,9 @@ module.exports =
       })
     }
 
-    // take 'iq' infor query messages and turn them into local state
-    info(stanza) {
-      let vCard = stanza.getChild('vCard')
-      if (vCard) {
-        this.profile = {}
-        vCard.children.forEach((field) => this.profile[field.name] = field.getText())
-      }
-    }
-
-    listen() {
+    listen () {
       const client = this.connect()
+      let backToServer = new Rx.Subject()
       this.subscription =
         Rx.Observable.merge(
           // online? go and get the profile for the bot
@@ -53,17 +47,51 @@ module.exports =
           ,
           // stanzas are messages from the server
           Rx.Observable.fromEvent(client, 'stanza')
-            .map((stanza) => {
+            .do((stanza) => {
+              // informational queries come back with an online status
               if (Object.is(stanza.name, 'iq')) {
-                this.info(stanza)
-                return new XmppClient.Stanza('presence', {})
-                  .c('show').t('chat').up()
-                  .c('status').t(this.options.status || '')
+                let vCard = stanza.getChild('vCard')
+                if (vCard) {
+                  this.profile = {}
+                  vCard.children.forEach((field) => this.profile[field.name] = field.getText())
+                }
+                backToServer.onNext(
+                  new XmppClient.Stanza('presence', {})
+                    .c('show').t('chat').up()
+                    .c('status').t(this.options.status || ''))
               }
-
-              console.log(JSON.stringify(stanza))
-              return {}
+              // messages add to a dialog session
+              // Message without body is probably a typing notification, but in any case
+              // there is not much to say in response
+              if (Object.is(stanza.name, 'message') && Object.is(stanza.attrs.type, 'chat') && stanza.getChildText('body')) {
+                stanza.to = new XmppClient.JID(stanza.attrs.to)
+                stanza.from = new XmppClient.JID(stanza.attrs.from)
+                stanza.text = stanza.getChildText('body')
+                let ses = new botframework.Session({
+                  localizer: this.options.localizer,
+                  dialogs: this,
+                  dialogId: this.options.defaultDialogId,
+                  dialogArgs: {}
+                })
+                // precreated since the memory store isn't actually async, this is where
+                // it is time to send a message back up to the server
+                ses.on('send', (msg) => {
+                  backToServer.onNext(new XmppClient.Stanza('message', {to: stanza.from, type: 'chat'})
+                    .c('body').t(msg.text))
+                })
+                this.options.sessionStore.get(stanza.from, (err, data) => {
+                  if (err)
+                    ses.dispatch(null, stanza)
+                  else
+                    ses.dispatch(data, stanza)
+                })
+              }
             })
+            // all processed, do not forward stanzas from the server, anything to go back to the server
+            // needs to be `backToServer.onNext`
+            .filter(() => false)
+          ,
+          backToServer
         )
           // events making it out to here are stanzas to send along to the server
           .filter((isStanza) => isStanza instanceof XmppClient.Element)
@@ -75,4 +103,4 @@ module.exports =
           .subscribe()
     }
 
-  }
+}
